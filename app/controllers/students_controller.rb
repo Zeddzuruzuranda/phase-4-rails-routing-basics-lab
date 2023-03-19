3069b9c374accfa45dc0e11137e1e46a44de47a4
Zeddzuruzuranda <raphomelvo@gmail.com>
2023-03-19 17:12:50 +0300

    completed

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,15 +1,15 @@
 class StudentsController < ApplicationController
     def index
-        @students = students.all
-        render "index"
+        students = Student.all
+        render json: students   
     end
     def grades
-        @students = Student.order(grade:  :desc)
-        render json: @students
+        students = Student.all.order(grade: :desc)
+        render json: students
     end
 
     def highest_grades
-        @highest_grades = Students.order(grade: :desc).first
-        render json: student
+        students = Student.all.order(grade: :desc).first
+        render json: students
     end
 end
